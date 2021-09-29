6dcc491647e8c3c68867b3c603ca039dd2fb840b
Xi <xwei@utexas.edu>
2021-09-29 14:27:03 +0800

    Testing MuxStatsExoPlayer with MockK

--- app/src/test/java/com/xizz/mockmuxstatsexoplayer/ExampleUnitTest.kt
+++ app/src/test/java/com/xizz/mockmuxstatsexoplayer/ExampleUnitTest.kt
@@ -1,16 +1,14 @@
 package com.xizz.mockmuxstatsexoplayer
 
-import org.junit.Assert.assertEquals
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.mux.stats.sdk.muxstats.MuxStatsExoPlayer
+import io.mockk.mockk
 import org.junit.Test
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun test() {
+        val simpleExoPlayer = mockk<SimpleExoPlayer>(relaxed = true)
+        val muxStatsExoPlayer = mockk<MuxStatsExoPlayer>(relaxed = true)
     }
 }
